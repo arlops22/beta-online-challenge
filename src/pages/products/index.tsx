baf782cac80785a14f66fad6285e773687e5481c
arlops22 <ary.s.lopes22@gmail.com>
2024-03-10 23:14:45 -0300

    product list filters

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { Button, IconButton, Typography } from '@material-tailwind/react';
+import ReactPaginate from 'react-paginate';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/16/solid';
 
 import getAllProducts, { IProductData } from '@/api/products/getAllProducts';
 import { IProduct } from '@/types';
 
 import ProductCard from '@/components/ProductCard';
 import SearchBar from '@/components/SearchBar';
-import ReactPaginate from 'react-paginate';
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/16/solid';
 
 export const getServerSideProps = (async () => {
   const data = await getAllProducts();
@@ -18,12 +18,25 @@ export const getServerSideProps = (async () => {
 
 export default function Products(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
     const { data } = props;
-    console.log('data', data)
+    
     const [search, setSearch] = useState('');
     const [page, setPage] = useState(1);
 
     const onPageChange = (selectedItem: { selected: number }) => {
       setPage(selectedItem.selected + 1);
+    }
+
+    const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value);
+    }
+
+    const getFilteredProducts = (products: IProduct[]) => {
+        return products
+                .slice((page - 1) * 6, page * 6)
+                .filter((product) => {
+                    return product.title.toLowerCase().includes(search.toLowerCase()) ||
+                           product.category.toLowerCase().includes(search.toLowerCase());
+                });
     }
 
     return (
@@ -33,7 +46,7 @@ export default function Products(props: InferGetServerSidePropsType<typeof getSe
         <div className="flex items-center justify-between mb-8">
           <SearchBar 
             value={search}
-            onChange={(e) => setSearch(e.target.value)}
+            onChange={onSearch}
             placeholder='Pesquisar produtos'
           />
           <Button 
@@ -44,12 +57,13 @@ export default function Products(props: InferGetServerSidePropsType<typeof getSe
         </div>
 
         <div className="flex items-center flex-wrap -mx-3">
-          {data.products.map((product: IProduct) => (
+          {getFilteredProducts(data.products).map((product: IProduct) => (
             <div 
-              className="w-1/3 px-3 mb-6"
+              className="lg:w-1/3 w-1/2 px-3 mb-6"
               key={product.id}
             >
               <ProductCard 
+                productId={product.id}
                 title={product.title}
                 description={product.description}
                 thumbnail={product.thumbnail}
