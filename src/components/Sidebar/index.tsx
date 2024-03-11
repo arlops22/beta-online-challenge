c564b4e02768544e63ef01d788d46abe2ab61ac7
arlops22 <ary.s.lopes22@gmail.com>
2024-03-10 23:24:40 -0300

    icons on sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,4 @@
+import { HomeIcon, PowerIcon, ShoppingBagIcon } from '@heroicons/react/16/solid';
 import { 
     Card, 
     List, 
@@ -13,32 +14,46 @@ const SideBar = () => {
 
     return (
         <Card className="fixed h-screen w-72 p-4 shadow-xl shadow-blue-gray-900/5">
-            <div className="mb-2 p-4">
-                <Typography 
-                    variant="h5" 
-                    color="blue-gray"
-                >
-                    Beta Online
-                </Typography>
+            <div className="flex flex-col justify-between h-full">
+                <div>
+                    <div className="mb-2 p-4">
+                        <Typography 
+                            variant="h5" 
+                            color="blue-gray"
+                        >
+                            Beta Online
+                        </Typography>
+                    </div>
+                    <List>
+                        <Link href='/'>
+                            <ListItem selected={router.asPath === '/'}>
+                                <ListItemPrefix>
+                                    <HomeIcon className="h-5 w-5" />
+                                </ListItemPrefix>
+                                Home
+                            </ListItem>
+                        </Link>
+                        <Link href='/products'>
+                            <ListItem selected={router.asPath === '/products'}>
+                                <ListItemPrefix>
+                                    <ShoppingBagIcon className="h-5 w-5" />
+                                </ListItemPrefix>
+                                Produtos
+                            </ListItem>
+                        </Link>
+                    </List>
+                </div>
+                    <List>
+                        <Link href='/'>
+                            <ListItem className='text-red-400 hover:text-red-400' selected={router.asPath === '/'}>
+                                <ListItemPrefix>
+                                    <PowerIcon className="h-5 w-5 text-red-400" />
+                                </ListItemPrefix>
+                                Sair
+                            </ListItem>
+                        </Link>
+                    </List>
             </div>
-            <List>
-                <Link href='/'>
-                    <ListItem selected={router.asPath === '/'}>
-                        {/* <ListItemPrefix>
-                            <PresentationChartBarIcon className="h-5 w-5" />
-                        </ListItemPrefix> */}
-                        Home
-                    </ListItem>
-                </Link>
-                <Link href='/products'>
-                    <ListItem selected={router.asPath === '/products'}>
-                        {/* <ListItemPrefix>
-                            <PresentationChartBarIcon className="h-5 w-5" />
-                        </ListItemPrefix> */}
-                        Produtos
-                    </ListItem>
-                </Link>
-            </List>
         </Card>
     )
 }
